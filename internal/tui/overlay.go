060dfc4c5b267defeb58cc249bb0035c561a1a65
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-06-07 15:11:29 +0300

    feat(tui): add overlay

---
+++ internal/tui/overlay.go
@@ -0,0 +1,124 @@
+// This code is from https://github.com/charmbracelet/lipgloss/pull/102 & https://github.com/yorukot/superfile
+package tui
+
+import (
+	"strings"
+
+	charmansi "github.com/charmbracelet/x/ansi"
+	ansi "github.com/muesli/reflow/ansi"
+	"github.com/muesli/reflow/truncate"
+	"github.com/muesli/termenv"
+)
+
+type whitespace struct {
+	style termenv.Style
+	chars string
+}
+
+type WhitespaceOption func(*whitespace)
+
+func (w whitespace) render(width int) string {
+	if w.chars == "" {
+		w.chars = " "
+	}
+
+	r := []rune(w.chars)
+	j := 0
+	b := strings.Builder{}
+
+	// Cycle through runes and print them into the whitespace.
+	for i := 0; i < width; {
+		b.WriteRune(r[j])
+		j++
+		if j >= len(r) {
+			j = 0
+		}
+		i += charmansi.StringWidth(string(r[j]))
+	}
+
+	// Fill any extra gaps white spaces. This might be necessary if any runes
+	// are more than one cell wide, which could leave a one-rune gap.
+	short := width - charmansi.StringWidth(b.String())
+	if short > 0 {
+		b.WriteString(strings.Repeat(" ", short))
+	}
+
+	return w.style.Styled(b.String())
+}
+
+// placeOverlay places fg on top of bg.
+func placeOverlay(x, y int, fg, bg string, opts ...WhitespaceOption) string {
+	fgLines, fgWidth := getLines(fg)
+	bgLines, bgWidth := getLines(bg)
+	bgHeight := len(bgLines)
+	fgHeight := len(fgLines)
+
+	if fgWidth >= bgWidth && fgHeight >= bgHeight {
+		// FIXME: return fg or bg?
+		return fg
+	}
+	// TODO: allow placement outside of the bg box?
+	x = clamp(x, 0, bgWidth-fgWidth)
+	y = clamp(y, 0, bgHeight-fgHeight)
+
+	ws := &whitespace{}
+	for _, opt := range opts {
+		opt(ws)
+	}
+
+	var b strings.Builder
+	for i, bgLine := range bgLines {
+		if i > 0 {
+			b.WriteByte('\n')
+		}
+		if i < y || i >= y+fgHeight {
+			b.WriteString(bgLine)
+			continue
+		}
+
+		pos := 0
+		if x > 0 {
+			left := truncate.String(bgLine, uint(x))
+			pos = ansi.PrintableRuneWidth(left)
+			b.WriteString(left)
+			if pos < x {
+				b.WriteString(ws.render(x - pos))
+				pos = x
+			}
+		}
+
+		fgLine := fgLines[i-y]
+		b.WriteString(fgLine)
+		pos += ansi.PrintableRuneWidth(fgLine)
+
+		right := charmansi.TruncateLeft(bgLine, pos, "")
+		bgWidth = ansi.PrintableRuneWidth(bgLine)
+		rightWidth := ansi.PrintableRuneWidth(right)
+		if rightWidth <= bgWidth-pos {
+			b.WriteString(ws.render(bgWidth - rightWidth - pos))
+		}
+
+		b.WriteString(right)
+	}
+
+	return b.String()
+}
+
+func clamp(v, lower, upper int) int {
+	return min(max(v, lower), upper)
+}
+
+// Split a string into lines, additionally returning the size of the widest
+// line.
+func getLines(s string) ([]string, int) {
+	lines := strings.Split(s, "\n")
+	widest := 0
+	for _, l := range lines {
+		w := charmansi.StringWidth(l)
+		if widest < w {
+			widest = w
+		}
+	}
+
+	return lines, widest
+}
