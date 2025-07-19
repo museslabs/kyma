519eb6041ea0506b98ba4c61d67815c15a05adb9
ploMP4 <artopouloskon@gmail.com>
2025-07-19 20:09:28 +0300

    feat(markdown): parse image dimensions in image_parser

--- internal/markdown/image_parser.go
+++ internal/markdown/image_parser.go
@@ -2,6 +2,9 @@ package markdown
 
 import (
 	"bytes"
+	"fmt"
+	"log/slog"
+	"strconv"
 	"strings"
 )
 
@@ -15,39 +18,108 @@ func (p ImageParser) Trigger() []byte {
 	return []byte{'!'}
 }
 
+// Parse attempts to extract an [ImageNode] from the input, matching the markdown
+// image syntax: [alt text|widthxheight](image-path) where width and height
+// values are optional.
 func (p ImageParser) Parse(r *bytes.Reader) Node {
 	var altText bytes.Buffer
 
-	altByte, err := r.ReadByte()
-	if err != nil || altByte != '[' {
+	b, err := r.ReadByte()
+	if err != nil || b != '[' {
+		slog.Warn("failed to parse image node")
 		return nil
 	}
 
-	for altByte != ']' {
-		altByte, err = r.ReadByte()
+	for b != ']' && b != '|' {
+		altText.WriteByte(b)
+
+		b, err = r.ReadByte()
 		if err != nil {
+			slog.Warn("failed to advance reader", slog.Any("error", err))
 			return nil
 		}
-		altText.WriteByte(altByte)
+	}
+
+	width := 0
+	height := 0
+
+	if b == '|' {
+		width, height, err = p.parseDimensions(r)
+		if err != nil {
+			slog.Warn("failed to parse image dimensions", slog.Any("error", err))
+			return nil
+		}
 	}
 
 	var path bytes.Buffer
 
 	pathByte, err := r.ReadByte()
 	if err != nil || pathByte != '(' {
+		slog.Warn("failed to parse image node")
 		return nil
 	}
 
 	for pathByte != ')' {
+		path.WriteByte(pathByte)
+
 		pathByte, err = r.ReadByte()
 		if err != nil {
+			slog.Warn("failed to advance reader", slog.Any("error", err))
 			return nil
 		}
-		path.WriteByte(pathByte)
 	}
 
 	return &ImageNode{
-		Label: strings.Trim(altText.String(), "[]"),
-		Path:  strings.Trim(path.String(), "()"),
+		Label:  strings.Trim(altText.String(), "[]"),
+		Path:   strings.Trim(path.String(), "()"),
+		Width:  width,
+		Height: height,
 	}
+}
+
+// parseDimensions parses a dimension string in the format 20x10 from the reader
+// and returns the corresponding width and height.
+func (p ImageParser) parseDimensions(r *bytes.Reader) (int, int, error) {
+	var wbuf bytes.Buffer
+	var hbuf bytes.Buffer
+
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			return 0, 0, err
+		}
+		if b == 'x' {
+			break
+		} else if b >= '0' && b <= '9' {
+			wbuf.WriteByte(b)
+		} else {
+			return 0, 0, fmt.Errorf("invalid character found %c", b)
+		}
+	}
+
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			return 0, 0, err
+		}
+		if b == ']' {
+			break
+		} else if b >= '0' && b <= '9' {
+			hbuf.WriteByte(b)
+		} else {
+			return 0, 0, fmt.Errorf("invalid character found %c", b)
+		}
+	}
+
+	width, err := strconv.Atoi(wbuf.String())
+	if err != nil {
+		return 0, 0, err
+	}
+
+	height, err := strconv.Atoi(hbuf.String())
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return width, height, nil
 }
--- internal/markdown/image_parser_test.go
+++ internal/markdown/image_parser_test.go
@@ -24,6 +24,26 @@ func TestImageParser_Parse(t *testing.T) {
 			in:   []byte("[not_an_image(path.png)"),
 			want: nil,
 		},
+		{
+			name: "Image node with custom width and height",
+			in:   []byte("[alt text|20x10](./image.png)"),
+			want: &ImageNode{
+				Label:  "alt text",
+				Path:   "./image.png",
+				Width:  20,
+				Height: 10,
+			},
+		},
+		{
+			name: "Image node with invalid width and height syntax",
+			in:   []byte("[alt text|20,10](./image.png)"),
+			want: nil,
+		},
+		{
+			name: "Image node with invalid width and height params",
+			in:   []byte("[alt text|2x1z](./image.png)"),
+			want: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
